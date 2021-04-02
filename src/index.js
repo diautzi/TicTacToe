bfc32878a5d861378d8c5733bfac074b6cf58a53
Diana Utan <didiutzi@gmail.com>
2021-04-01 20:35:17 -0500

    created game component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// import components
+import Game from './components/Game'
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
